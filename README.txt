8e365ebc5e3ffaf129765767612163e9f5470472
Jon Elorriaga <jonelorriaga@iMac-de-Jon.local>
2021-11-30 11:02:50 +0100

    third

--- README.txt
+++
@@ -1,2 +0,0 @@
-peeeeerro
-tienes que borrrar ds_strore y poner los headers en: strmapi, todos los acabados en _fd y alguno mas igual
--- ft_substr.c
+++ ft_substr.c
@@ -9,21 +9,34 @@
 /*   Updated: 2021/11/12 20:03:55 by jelorria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *a = (char*)malloc(len);
-	char *s2 = (char*)s;
-	unsigned int i;
+	char				*dest;
+	size_t				i;
+	unsigned int		n;
 
+	if (!s)
+		return (NULL);
+	n = ft_strlen(s);
+	if (start > n)
+		return (ft_strdup(""));
+	if (len > n)
+		len = n;
+	dest = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!dest)
+		return (NULL);
 	i = 0;
-	while (len-- > 0)
+	if (n > start)
 	{
-		a[i] = s2[start];
-		i++;
-		start++;
+		while (s[start + i] && i < len)
+		{
+			dest[i] = s[start + i];
+			i++;
+		}
 	}
-	return (a);
+	dest[i] = '\0';
+	return (dest);
 }
